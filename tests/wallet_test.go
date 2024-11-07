bb16a3ba0ef94fd438049918e3d653eee959e58f
Coding迪斯尼 <chenyi.008@163.com>
2024-11-07 12:22:20 +0800

    add withdraw test

--- tests/wallet_test.go
+++ tests/wallet_test.go
@@ -39,10 +39,15 @@ func setup() {
 func TestDeposit(t *testing.T) {
 	setup()
 
-	// Step 1: Create a user (manually, or assume user creation is handled elsewhere)
-	userID := 1 // Assume the user is created with ID 100
+	//need to make sure user with id 1 exists
+	userID := 1
 
-	// Step 2: Prepare the request to deposit money
+	var currentDeposit decimal.Decimal
+	err := walletService.DB.QueryRow("SELECT balance FROM wallets WHERE user_id = $1", userID).Scan(&currentDeposit)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Prepare the request to deposit money
 	depositAmount := decimal.NewFromFloat(100.50)
 
 	body := map[string]string{
@@ -54,6 +59,8 @@ func TestDeposit(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	totalDeposit := currentDeposit.Add(depositAmount)
 
 	// Create a response recorder to capture the result
 	rr := httptest.NewRecorder()
@@ -76,7 +83,60 @@ func TestDeposit(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	isEqual := depositAmount.Equal(balance)
+	isEqual := totalDeposit.Equal(balance)
 	// Check if the deposit amount is correct
+	assert.Equal(t, true, isEqual)
+}
+
+func TestWithdraw(t *testing.T) {
+	setup()
+
+	// make sure user with id 1 exists
+	//and his balance is more than 50.00
+	userID := 1
+
+	//get his initial deposit
+	var initialDeposit decimal.Decimal
+	err := walletService.DB.QueryRow("SELECT balance FROM wallets WHERE user_id = $1", userID).Scan(&initialDeposit)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//Prepare the request to withdraw money
+	withdrawAmount := decimal.NewFromFloat(50.00)
+	body := map[string]string{
+		"amount": withdrawAmount.String(),
+	}
+	bodyJSON, _ := json.Marshal(body)
+	userStr := fmt.Sprintf("%d", userID)
+	req, err := http.NewRequest(http.MethodPost, "/wallet/"+userStr+"/withdraw", bytes.NewReader(bodyJSON))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a response recorder to capture the result
+	rr := httptest.NewRecorder()
+
+	// Create a new Gin engine
+	router := gin.Default()
+	router.POST("/wallet/:user_id/withdraw", walletHandler.Withdraw)
+
+	// Step 3: Perform the request
+	router.ServeHTTP(rr, req)
+
+	// Step 4: Check the response
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.Contains(t, rr.Body.String(), "Withdrawal successful")
+
+	// Step 5: Check the updated balance
+	var balance decimal.Decimal
+	err = walletService.DB.QueryRow("SELECT balance FROM wallets WHERE user_id = $1", userID).Scan(&balance)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// The balance should be the initial deposit minus the withdrawal
+	expectedBalance := initialDeposit.Sub(withdrawAmount)
+	isEqual := expectedBalance.Equal(balance)
 	assert.Equal(t, true, isEqual)
 }
